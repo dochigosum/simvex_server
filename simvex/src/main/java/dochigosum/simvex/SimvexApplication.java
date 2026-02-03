39771327f389c1a67d6a4278a370c3e66c092b07
kusuri12 <waterfail12@naver.com>
2026-02-04 00:35:18 +0900

    chore: spring project setting(#2)

---
+++ simvex/src/main/java/dochigosum/simvex/SimvexApplication.java
@@ -0,0 +1,13 @@
+package dochigosum.simvex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimvexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimvexApplication.class, args);
+    }
+
+}
---
+++ simvex/src/test/java/dochigosum/simvex/SimvexApplicationTests.java
@@ -0,0 +1,13 @@
+package dochigosum.simvex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimvexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
