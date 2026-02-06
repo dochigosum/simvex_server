7e215fcc72508d6fb390a1b094061f11e378bf95
gh7035 <gh7035@daum.net>
2026-02-06 23:06:20 +0900

    chore: s3config를 properties 처리(#23)

--- simvex/src/main/java/dochigosum/simvex/global/config/s3/S3Config.java
+++ simvex/src/main/java/dochigosum/simvex/global/config/s3/S3Config.java
@@ -1,5 +1,6 @@
 package dochigosum.simvex.global.config.s3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
@@ -9,11 +10,14 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
+    @Value("${AWS_REGION}")
+    private String region;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of("ap-northeast-2")) // 반드시 region 지정
-                .credentialsProvider(AnonymousCredentialsProvider.create()) // 퍼블릭용
+                .region(Region.of(region))
+                .credentialsProvider(AnonymousCredentialsProvider.create())
                 .build();
     }
 }
--- simvex/src/main/java/dochigosum/simvex/global/config/s3/S3ReadService.java
+++ simvex/src/main/java/dochigosum/simvex/global/config/s3/S3ReadService.java
@@ -1,13 +1,16 @@
 package dochigosum.simvex.global.config.s3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class S3ReadService {
 
-    private final String bucket = "dochigosum-s3";
-    private final String region = "ap-northeast-2";
+    @Value("${BUCKET_NAME}")
+    private String bucket;
+    @Value("${AWS_REGION}")
+    private String region;
 
     public byte[] getGltfFile(String fileName) {
         String url = "https://" + bucket + ".s3." + region + ".amazonaws.com/3d_asset/" + fileName;
