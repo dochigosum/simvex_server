86fbd2e41f508029a249e52d18baf728369645d1
gh7035 <gh7035@daum.net>
2026-02-07 16:51:54 +0900

    feat: s3 업로드 로직 추가

---
+++ simvex/src/main/java/dochigosum/simvex/global/s3/S3Service.java
@@ -0,0 +1,110 @@
+package dochigosum.simvex.global.s3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Slf4j
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    @Value("${BUCKET_NAME}")
+    private String bucket;
+    @Value("${AWS_REGION}")
+    private String region;
+
+    public String getDrawingUrl(String drawing, String fileName) {
+        return "https://" +
+                bucket +
+                ".s3." +
+                region +
+                ".amazonaws.com/3d_asset/" +
+                drawing + "/" +
+                fileName;
+    }
+
+    // project/{member_id}/{project_name}/저장할파일명 => 유저의 프로젝트 파일명
+    public String getProjectImgUrl(String memberId, String projectName, String fileName) {
+        return "https://" +
+                bucket +
+                ".s3." +
+                region +
+                ".amazonaws.com/project/" +
+                memberId + "/" +
+                projectName + "/" +
+                fileName;
+    }
+
+    // preview/파일명
+    // getDrawingPreviewImgUrl
+    public String getDrawingPreviewImgUrl(String drawingName) {
+        return "https://" +
+                bucket +
+                ".s3." +
+                region +
+                ".amazonaws.com/drawing/" +
+                drawingName;
+    }
+
+    // uploadProjectImg
+    public String uploadProjectPreviewImg(String memberId, String projectName, String filePath) {
+        Path path = Paths.get(filePath);
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException("파일이 존재하지 않습니다: " + filePath);
+        }
+
+        String fileName = path.getFileName().toString();
+        String key = "project/" + memberId + "/" + projectName + "/" + fileName;
+
+        try {
+            // 기존 파일 삭제 (멱등적)
+            deleteObject(key);
+
+            // Content-Type 자동 감지
+            String contentType = Files.probeContentType(path);
+
+            // 새 파일 업로드
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(contentType)
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromFile(path));
+            log.info("S3 업로드 완료: bucket={}, key={}", bucket, key);
+
+            return getProjectImgUrl(memberId, projectName, fileName);
+
+        } catch (IOException e) {
+            log.error("S3 업로드 실패: key={}, error={}", key, e.getMessage());
+            throw new RuntimeException("파일 업로드 실패: " + e.getMessage(), e);
+        }
+    }
+
+    private void deleteObject(String key) {
+        try {
+            DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(deleteRequest);
+        } catch (Exception e) {
+            log.debug("S3 삭제 실패 : key={}, error={}", key, e.getMessage());
+        }
+    }
+
+}
