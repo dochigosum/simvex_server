7d36f1ece375f0627700d167246e55c0daa5ab39
gh7035 <gh7035@daum.net>
2026-02-07 11:14:06 +0900

    refactor: change S3 access to return public file URLs(#23)

--- simvex/src/main/java/dochigosum/simvex/global/config/s3/S3Config.java
+++ simvex/src/main/java/dochigosum/simvex/global/config/s3/S3Config.java
@@ -1,23 +1,17 @@
 package dochigosum.simvex.global.config.s3;
 
+import io.awspring.cloud.autoconfigure.s3.S3ClientCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
-    @Value("${AWS_REGION}")
-    private String region;
-
     @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(AnonymousCredentialsProvider.create())
-                .build();
+    public S3ClientCustomizer s3ClientCustomizer() {
+        return builder -> builder
+                .credentialsProvider(AnonymousCredentialsProvider.create());
     }
 }
--- simvex/src/main/java/dochigosum/simvex/global/config/s3/S3ReadService.java
+++ simvex/src/main/java/dochigosum/simvex/global/config/s3/S3ReadService.java
@@ -12,9 +12,13 @@ public class S3ReadService {
     @Value("${AWS_REGION}")
     private String region;
 
-    public byte[] getGltfFile(String fileName) {
-        String url = "https://" + bucket + ".s3." + region + ".amazonaws.com/3d_asset/" + fileName;
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(url, byte[].class);
+    public String getGltfUrl(String folder,String fileName) {
+        return "https://" +
+                bucket +
+                ".s3." +
+                region +
+                ".amazonaws.com/" +
+                folder + "/" +
+                fileName;
     }
 }
