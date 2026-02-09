d5c4a22f7cba81c0bb146d836e080c32f88f7f0d
Kim Minseo <u01056305988@gmail.com>
2026-02-10 00:10:42 +0900

    chore: projectController에 오타 수정 #17

--- simvex/src/main/java/dochigosum/simvex/domain/project/presentation/ProjectController.java
+++ simvex/src/main/java/dochigosum/simvex/domain/project/presentation/ProjectController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/projects")
+@RequestMapping("/api/v1/project")
 @RequiredArgsConstructor
 public class ProjectController {
 
