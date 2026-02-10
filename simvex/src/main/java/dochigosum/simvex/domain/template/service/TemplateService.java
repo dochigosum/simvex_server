4ef13a689296ec79e9287cb6b2662ee844d40a54
kusuri12 <waterfail12@naver.com>
2026-02-10 19:30:08 +0900

    chore: swagger 추가 및 redis 구현(#55)

--- simvex/src/main/java/dochigosum/simvex/domain/template/service/TemplateService.java
+++ simvex/src/main/java/dochigosum/simvex/domain/template/service/TemplateService.java
@@ -9,6 +9,8 @@ import dochigosum.simvex.domain.template.repository.DrawingTemplateRepository;
 import dochigosum.simvex.domain.template.repository.PartTemplateRepository;
 import dochigosum.simvex.global.error.exception.SimvexException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,13 +19,17 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class TemplateService {
 
     private final DrawingTemplateRepository drRepo;
     private final PartTemplateRepository partRepo;
 
     // 조립도 목록 조회
+    @Cacheable(value = "drawing", key = "'all'", cacheManager = "cacheManager")
     public List<DrawingListResponse> getDrawings() {
+        log.info("Redis 캐시에 데이터가 없어서 DB에서 drawing_template 리스트를 조회합니다.");
+
         return drRepo.findAll()
                 .stream()
                 .map(DrawingListResponse::from)
@@ -49,7 +55,10 @@ public class TemplateService {
     }
 
     // 조립도 별 부품 목록 조회
+    @Cacheable(value = "parts", key = "#id", cacheManager = "cacheManager")
     public List<PartListResponse> getParts(Long id) {
+        log.info("Redis 캐시에 데이터가 없어서 DB에서 drawing_template id {} 에 해당하는 part_template 리스트를 조회합니다.", id);
+
         return partRepo.findAllByDrawingTemplate_Id(id)
                 .stream()
                 .map(PartListResponse::from)
--- simvex/src/main/java/dochigosum/simvex/global/config/RedisConfig.java
+++ simvex/src/main/java/dochigosum/simvex/global/config/RedisConfig.java
@@ -1,16 +1,28 @@
 package dochigosum.simvex.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
+@EnableCaching
 public class RedisConfig {
+
+    private static final Duration TTL = Duration.ofHours(1);
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -30,8 +42,29 @@ public class RedisConfig {
 
         // JSON 직렬화 설정
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(customSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean(name = "cacheManager")
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(customSerializer()))
+                .entryTtl(TTL) // 기본 캐시 유지 시간 (1시간)
+                .disableCachingNullValues(); // null 값은 캐싱하지 않음
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+    private GenericJackson2JsonRedisSerializer customSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
     }
 }
